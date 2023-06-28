5d786951f29c404e9ab6b52ed230d39a1d07f387
changedragon2 <1301792688@qq.com>
2023-06-28 18:20:01 +0800

    Nvchad和lazyvim管理neovim插件

---
+++ nvim/new/lua/plugins/configs/others.lua
@@ -0,0 +1,87 @@
+local M = {}
+local utils = require "core.utils"
+
+-- indent blankline color
+vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+vim.opt.list = true
+--vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
+M.blankline = {
+  indentLine_enabled = 1,
+  filetype_exclude = {
+    "help",
+    "terminal",
+    "lazy",
+    "lspinfo",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "mason",
+    "nvdash",
+    "nvcheatsheet",
+    "",
+  },
+    buftype_exclude = { "terminal" },
+    show_trailing_blankline_indent = false,
+    show_first_indent_level = false,
+    show_current_context = true,
+    show_current_context_start = true,
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+}
+
+M.luasnip = function(opts)
+  require("luasnip").config.set_config(opts)
+
+  -- vscode format
+  require("luasnip.loaders.from_vscode").lazy_load()
+  require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+  -- snipmate format
+  require("luasnip.loaders.from_snipmate").load()
+  require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+
+  -- lua format
+  require("luasnip.loaders.from_lua").load()
+  require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+  vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = function()
+      if
+        require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require("luasnip").session.jump_active
+      then
+        require("luasnip").unlink_current()
+      end
+    end,
+  })
+end
+
+M.gitsigns = {
+  signs = {
+    add = { text = "│" },
+    change = { text = "│" },
+    delete = { text = "󰍵" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "│" },
+  },
+  on_attach = function(bufnr)
+    utils.load_mappings("gitsigns", { buffer = bufnr })
+  end,
+}
+
+return M
